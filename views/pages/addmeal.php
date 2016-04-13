6b7083118f071674c5557595085211e1243bbc87
marklandp <marklandpayne@gmail.com>
2016-04-13 12:51:11 -0500

    updated views

--- views/pages/addmeal.php
+++ views/pages/addmeal.php
@@ -4,6 +4,7 @@
 <button id="genPlan" type="button" class="btn btn-success">Generate Meal Plan</button>
 </div>
 <div id="selectIngredients">
+    <h1 class="heading">Select ingredients from below</h1>
 <?php
     if (isset($_SESSION['ingredients'])){
         echo '<form action="?controllers=control&action=createmeal" name="ingredientsList" method="post">';
@@ -18,5 +19,18 @@
 ?>
 </div>
 <div id="genmPlan">
-twfvjufvavyuc
+    <h1 class="heading">Select options that suit your needs</h1>
+<?php
+   if (isset($_SESSION['recipes'])){
+        echo '<form action="?controllers=control&action=generatePlan" name="recipeList" method="post">';
+        $recipes = $_SESSION['recipes'];
+        foreach ($recipes as $recipe){
+            echo '<input type="checkbox" name="ingredients[]" value="'.$recipe.'"/>'.$recipe.'';
+        }
+        echo '<input type="submit" name="formSubmit" value="Submit" /></form>';
+    } else {
+        echo '<h3>No matching recipes based on selected ingredients</h3>';
+    } 
+    
+?>
 </div> 
