0fe00db956d553270d17bd56c8a2da75fd73bcda
marklandp <marklandpayne@gmail.com>
2016-04-13 12:31:56 -0500

    added create meal page

--- views/pages/addmeal.php
+++ views/pages/addmeal.php
@@ -1,5 +1,9 @@
-<h1 class="heading">Ingredients</h1>
-
+<div id="toChoose">
+<h1 class="heading">what would you like to do?</h1>
+<button id="addmeal" type="button" class="btn btn-success">Add a Meal</button>
+<button id="genPlan" type="button" class="btn btn-success">Generate Meal Plan</button>
+</div>
+<div id="selectIngredients">
 <?php
     if (isset($_SESSION['ingredients'])){
         echo '<form action="?controllers=control&action=createmeal" name="ingredientsList" method="post">';
@@ -12,5 +16,7 @@
         echo '<h3>No ingredients in database</h3>';
     }
 ?>
-
- 
+</div>
+<div id="genmPlan">
+twfvjufvavyuc
+</div> 
